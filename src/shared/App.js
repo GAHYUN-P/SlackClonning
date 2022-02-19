1bac626a068aba7daff88196a441af482ff13a2a
hantaehoon1 <hantaehun1@naver.com>
2022-02-19 12:31:38 +0900

    first

--- src/shared/App.js
+++ src/shared/App.js
@@ -3,11 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import styled from "styled-components";
 import "../App.css";
 import LoginForm from '../components/LoginForm';
-
+import Header from "./Header";
 function App() {
 
   return (
     <div className="App">
+      <Header></Header>
       <Switch>
         <Route path="/login" exact component={LoginForm} />
       </Switch>
---
+++ src/shared/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Grid, Image} from '../elements'
+
+const Header = (props) => {
+    return (
+      
+        <Grid width="100%" height="10%" BG_c="#350d36">dddddd</Grid>
+        
+    );
+}
+
+export default Header
